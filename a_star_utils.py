8a739f0d53891de216f29644f2b4407810a8e41b
netta.barak <netta.barak@cs.huji.ac.il>
2019-08-07 11:48:13 +0300

    added bonuses as subgoals

--- a_star_utils.py
+++ a_star_utils.py
@@ -9,8 +9,25 @@ def g_function_by_score_and_time(game):
     return - (game.get_score() + game.time_left)
 
 
+def is_sub_goal_steps_score_bonuses(game, start):
+    return is_sub_goal_by_bonuses(game, start) or is_sub_goal_score_or_steps(game, start)
+
+
+def is_sub_goal_by_bonuses(game, start):
+    return is_sub_goal_life_bonus(game, start) or is_sub_goal_time_bonus(game, start)
+
+
+def is_sub_goal_life_bonus(game, start):
+    return start.players[0].lives < game.players[0].lives
+
+
+def is_sub_goal_time_bonus(game, start):
+    return start.time_left < game.time_left
+
+
 def is_sub_goal_by_score(game, start):
     return game.get_score() > start.get_score()
+
 
 def is_sub_goal_score_or_steps(game, start):
     return is_sub_goal_by_score(game, start) or is_sub_goal_by_steps(game, start)
@@ -18,8 +35,9 @@ def is_sub_goal_score_or_steps(game, start):
 def is_sub_goal_by_blow_up_ball(game, start):
     return calc_worth_of_balls(game) < calc_worth_of_balls(start)
 
+
 def is_sub_goal_by_steps(game, start):
-    print(start.time_left - game.time_left)
+    # print(start.time_left - game.time_left)
     return start.time_left - game.time_left >= 0.7
 
 # TODO implement efficient
