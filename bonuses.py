48681982cb6ad4eeea0edb26b096343f5dd2d9b1
netta.barak <netta.barak@cs.huji.ac.il>
2019-08-13 17:48:33 +0300

    documented bonuses

--- bonuses.py
+++ bonuses.py
@@ -9,14 +9,29 @@ bonus_types = [BONUS_LIFE, BONUS_TIME]
 
 
 class Bonus(pygame.sprite.Sprite):
+    """
+    this class represents a bonus in the game. a bonus can be dropped to the ground when a bubble is blown up. the
+    player can take it and get extra life\time
+    """
     def __init__(self, x, y, type):
+        """
+        :param x: x axis location
+        :param y: y axis location
+        :param type: 'bonus life' or 'bonus time'
+        """
         self.image = pygame.image.load(IMAGES_PATH + type + '.png')
         self.rect = self.image.get_rect(centerx=x, centery=y)
         self.type = type
 
     def update(self):
+        """
+        moves the bonus down if the it didn't get to the floor yet
+        """
         if self.rect.bottom < WINDOWHEIGHT:
             self.rect = self.rect.move(0, BONUS_SPEED)
 
     def deep_copy_bonus(self):
+        """
+        :return: a deepcopy
+        """
         return Bonus(self.rect.centerx, self.rect.centery, self.type)
