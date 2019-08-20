e5702a9b51758c4c3b503d9096df1f29e9ec5ed7
netta.barak <netta.barak@cs.huji.ac.il>
2019-08-20 20:20:44 +0300

    weapon doc

--- weapon.py
+++ weapon.py
@@ -4,7 +4,7 @@ from copy import copy, deepcopy
 
 
 class Weapon(pygame.sprite.Sprite):
-
+    """ a class representing a player's weapon"""
     def __init__(self, x=0, y=0):
         self.is_active = False
         pygame.sprite.Sprite.__init__(self)
@@ -12,9 +12,18 @@ class Weapon(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(centerx=x, top=y)
 
     def __eq__(self, other):
+        """
+        comperator
+        :param other:
+        :return: True if weapons are identical and False otherwise.
+        """
         return self.rect.centerx == other.rect.centerx and self.rect.top == other.rect.top
 
     def update(self):
+        """
+        updates the location and activeness of the weapon object
+        :return:
+        """
         if self.is_active:
             if self.rect.top <= 0:
                 self.is_active = False
@@ -22,6 +31,10 @@ class Weapon(pygame.sprite.Sprite):
                 self.rect = self.rect.move(0, -WEAPON_SPEED)
 
     def deep_copy_weapon(self):
+        """
+
+        :return: a deep copy of the given weapon
+        """
         copy_weapon = Weapon(self.rect.centerx, self.rect.top)
         copy_weapon.is_active = self.is_active
         return copy_weapon
